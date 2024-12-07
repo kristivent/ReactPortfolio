1a3fa32fcc766df9a488b582326971f7faa65e04
kristivent <ventk84@gmail.com>
2024-12-07 15:23:26 -0500

    Added bootstrap styles and npm, updated main.tsx and components

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,10 +3,20 @@ import developer from '../assets/images/developer.jpg';
 
 const AboutMe: React.FC = () => {
   return (
-    <section id="about-me">
-      <h2>About Me</h2>
-      <img src={developer} alt="Developer" />
-      <p>Current high school English teacher and department chair with 18 years experience. Recent Coding and Programming BootCamp graduate.  Looking to make career change and/or gain experience in a technology-based career path.</p>
+    <section id="about-me" className="container my-5">
+      <h2 className="text-primary mb-4">About Me</h2>
+      <div className="row">
+        <div className="col-md-4">
+          <img src={developer} alt="Developer" className="img-fluid rounded-circle" />
+        </div>
+        <div className="col-md-8">
+          <p>Current high school English teacher and department chair with 18 years experience.</p>
+          <p>M.S.Ed. in Education - Instructional Technology</p>
+          <p>B.S.Ed. in Secondary Education - English</p>
+          <p>Currently enrolled in Coding and Programming BootCamp.</p>
+          <p>Looking to make a career change and/or gain experience in a technology-based career path.</p>
+        </div>
+      </div>
     </section>
   );
 };
