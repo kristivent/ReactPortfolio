7b602361e8cdee0ed2895c7d995b008bba8ac63e
kristivent <ventk84@gmail.com>
2024-12-08 11:51:02 -0500

    updated App.tsx to help with error messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-// src/App.tsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
